40c5c496784b2c88dec12bf7ed1af37cef87a23a
Ser0jka123 <sersur42@yandex.ru>
2025-12-25 00:06:23 +0700

    upd

--- src/components/common/hero-block/hero-block.tsx
+++ src/components/common/hero-block/hero-block.tsx
@@ -4,8 +4,8 @@ import styles from './styles.module.scss';
 
 // import { MediaPlayer, MediaProvider } from '@vidstack/react';
 // import { defaultLayoutIcons, DefaultVideoLayout } from '@vidstack/react/player/layouts/default';
-import '@vidstack/react/player/styles/default/theme.css';
-import '@vidstack/react/player/styles/default/layouts/video.css';
+// import '@vidstack/react/player/styles/default/theme.css';
+// import '@vidstack/react/player/styles/default/layouts/video.css';
 
 export default function HeroBlock() {
   return (
