c92712f7fdd49d85c289cde2ae97a19133dd5d87
yumkie <masanaguy@gmail.com>
2026-04-22 16:32:47 +0700

    fix

--- src/app/vacancies/page.tsx
+++ src/app/vacancies/page.tsx
@@ -1,6 +1,7 @@
 import { VacancyCard } from '@/components/vacancyCard/vacancyCard';
 import { vacancies } from '@/data/vacancies.data';
 import styles from './styles.module.scss';
+import Contacts from '@/components/common/contacts/contacts';
 export async function generateMetadata() {
   return {
     title: "Вакансии стоматологии | Тридцать Два",
@@ -39,19 +40,49 @@ export async function generateMetadata() {
 }
 export default function VacanciesPage() {
   return (
-    <main className={styles.container}>
+    // <main className={styles.container}>
+    //   <h1 className={styles.pageTitle}>Наши вакансии</h1>
+    //   <div className={styles.grid}>
+    //     {vacancies.map((vacancy) => (
+    //       <VacancyCard
+    //         key={vacancy.id}
+    //         id={vacancy.id}
+    //         title={vacancy.title}
+    //         slug={vacancy.slug}
+    //         shortDescription={vacancy.shortDescription}
+    //       />
+    //     ))}
+    //   </div>
+    // </main>
+       <main className={styles.container}>
       <h1 className={styles.pageTitle}>Наши вакансии</h1>
       <div className={styles.grid}>
         {vacancies.map((vacancy) => (
-          <VacancyCard
-            key={vacancy.id}
-            id={vacancy.id}
-            title={vacancy.title}
-            slug={vacancy.slug}
-            shortDescription={vacancy.shortDescription}
-          />
+          <div key={vacancy.id} className={styles.card}>
+            <h2 className={styles.title}>{vacancy.title}</h2>
+            <section className={styles.section}>
+              <h3 className={styles.sectionTitle}>Что мы ждём от Вас:</h3>
+              <ul className={styles.list}>
+                {vacancy.fullContent.requirements.map((item, index) => (
+                  <li key={index} className={styles.listItem}>{item}</li>
+                ))}
+              </ul>
+            </section>
+            <section className={styles.section}>
+              <h3 className={styles.sectionTitle}>Мы предлагаем:</h3>
+              <ul className={styles.list}>
+                {vacancy.fullContent.offers.map((item, index) => (
+                  <li key={index} className={styles.listItem}>{item}</li>
+                ))}
+              </ul>
+            </section>
+            {vacancy.fullContent.note && (
+              <p className={styles.note}>{vacancy.fullContent.note}</p>
+            )}
+          </div>
         ))}
       </div>
+      <Contacts />
     </main>
   );
 }
